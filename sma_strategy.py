6a87b9492eb1a105f1997068249f74d9515df111
sunilsala88 <sunilsala88@gmail.com>
2024-06-30 19:04:24 +0530

    june30 7

--- sma_strategy.py
+++ sma_strategy.py
@@ -41,52 +41,19 @@ class Sma_Strategy(Strategy):
 # data=yf.download('GOOG',start='2020-06-24',end='2024-06-29',interval='1d')
 # print(data)
 
-from ib_insync import *
-# util.startLoop()  # uncomment this line when in a notebook
-import datetime as dt
-import pandas as pd
-ib = IB()
-ib.connect('127.0.0.1', 7497, clientId=66)
 
-def last_day_of_month(year, month):
-    next_month = month % 12 + 1
-    next_year = year + month // 12
-    last_day = dt.date(next_year, next_month, 1) - dt.timedelta(days=1)
-    return last_day
+data=pd.read_csv('GOOG_5min.csv')
+print(data)
+data['Date']=pd.to_datetime(data['Date'])
+data.set_index('Date',inplace=True)
+print(data)
 
-
-name='GOOG'
-contract2=Stock(name,'SMART','USD')
-# year=5
-final_data=pd.DataFrame()
-for month in range(1,6):
-
-    end_time=last_day_of_month(2023, month)
-    print(end_time)
-    bars = ib.reqHistoricalData(
-        contract2, endDateTime=end_time, durationStr='1 M',
-        barSizeSetting='5 mins', whatToShow='TRADES', useRTH=True,formatDate=2)
-    df1 = util.df(bars)
-    print(df1)
-    final_data=pd.concat([final_data,df1],axis=0).drop_duplicates()
-    
-print(final_data)
-
-d1={'date':'Date','open':'Open','high':'High','low':'Low','close':'Close','volume':'Volume'}
-final_data.rename(columns =d1, inplace = True) 
-final_data.set_index('Date',inplace=True)
-final_data.to_csv(f'{name}_5min.csv')
-data=final_data
-
-
-
-
-
-
+# data=data['2023-01-03 14:30:00':'2023-06-03 14:30:00']
+# print(data)
 bt = Backtest(data, Sma_Strategy,
               cash=10000, commission=.002,
               exclusive_orders=True)
 
 output = bt.run()
 print(output)
-bt.plot()
+# bt.plot()
